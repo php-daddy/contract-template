00f5a133630b455801051b2e110a1a0ab14da7fa
Nino Paolo Amarillento <o70vd@yahoo.com>
2015-03-15 01:02:52 +0800

    Added SingletonTrait

---
+++ src/SingletonTrait.php
@@ -0,0 +1,46 @@
+<?php namespace PhpDaddy\Contract\Singleton;
+
+trait SingletonTrait
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function getInstance()
+    {
+        static $instance = null;
+
+        if (null === $instance) {
+            $instance = new static();
+        }
+
+        return $instance;
+    }
+
+    /**
+     * Protected constructor to prevent creating a new instance of the
+     * *Singleton* via the `new` operator from outside of this class.
+     */
+    protected function __construct()
+    {
+    }
+
+    /**
+     * Private clone method to prevent cloning of the instance of the
+     * *Singleton* instance.
+     *
+     * @return void
+     */
+    private function __clone()
+    {
+    }
+
+    /**
+     * Private unserialize method to prevent unserializing of the *Singleton*
+     * instance.
+     *
+     * @return void
+     */
+    private function __wakeup()
+    {
+    }
+}
